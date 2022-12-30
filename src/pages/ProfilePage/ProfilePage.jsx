5044be6abad67a07e731b123e923ad8a55dda96a
ermolaevanton <ermolaevamton@gmail.com>
2022-12-30 23:13:06 +0300

    lesson-5

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,27 @@
-import styles from './style/ProfilePage.module.css'
+import styles from './style/ProfilePage.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 export function ProfilePage() {
+    const { toggle } = useSelector((store) => store);
+    const dispatch = useDispatch();
+    const setToggle = () => {
+        dispatch({ type: 'CHANGE_STATUS', payload: !toggle })
+    }
+
+
+
     return (
-        <h1 className={styles.ProfilePage}>Profile page</h1>
+        <>
+            <h1 className={styles.ProfilePage}>Profile page</h1>
+            <div className={styles.box}>
+                <input
+                    className={styles.checkbox}
+                    type="checkbox"
+                    onChange={setToggle}
+                />
+                <span className={styles.text}>{String(toggle)}</span>
+            </div>
+        </>
     )
 }
